95e02426e142b0231d9118d9688d2dd296c0675d
NaimurWD <naimur.wd@gmail.com>
2025-07-26 13:44:28 +0600

    new Page route create and link of next/link learned

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Home() {
   // console.log("executing"); // Loges in server
   return (
@@ -5,6 +7,10 @@ export default function Home() {
       <img src="/logo.png" alt="A server surrounded by magic sparkles." />
       <h1>Welcome to this NextJS Course!</h1>
       <p>🔥 Let&apos;s get started! 🔥</p>
+      <p>
+        {/* <a href="/about">About</a> removes from single page, So We need to use Link from next/link  */}
+        <Link href="/about">About</Link>
+      </p>
     </main>
   );
 }
