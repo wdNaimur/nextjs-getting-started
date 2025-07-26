4754d60b1e55c5e35f197b8546c2bc6d8754c6cb
NaimurWD <naimur.wd@gmail.com>
2025-07-26 13:33:59 +0600

    first commit

---
+++ app/page.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  // console.log("executing"); // Loges in server
+  return (
+    <main>
+      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
+      <h1>Welcome to this NextJS Course!</h1>
+      <p>🔥 Let&apos;s get started! 🔥</p>
+    </main>
+  );
+}
